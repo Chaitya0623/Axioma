e3987c06ae7732acc9598dceda78bb7d06c0ceaa
Chaitya0623 <chaitya0623@gmail.com>
2025-02-27 22:11:18 +0530

    Backend Deploy

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["https://axioma-six.vercel.app/", "https://newsroom-analytics-demo.vercel.app", "http://localhost:5173"],
+  origin: ["https://axioma-six.vercel.app", "https://newsroom-analytics-demo.vercel.app", "http://localhost:5173"],
   methods: ["GET", "POST"]
 }));
 app.use(bodyParser.json());
